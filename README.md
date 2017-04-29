e049b16ce462ec78c4c5ff3c06a1692988b0881e
houmingjie <houmingjie@douyu.tv>
2017-04-29 11:45:23 +0800

    commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# react-input-ie8
+See https://github.com/facebook/react/issues/3926
+
+React 14.*版本仍然支持IE8，该版本在输入中文时input组件的onChange判定处理的有问题。
+
+reference https://github.com/suhaotian/react-input
+
+## Usage
+just like React input but the onChange callback receive the current value as arguments。
---
+++ index.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+
+class Input extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            value: props.value,
+        };
+
+        this.onComposition = false;
+        this.handleComposition = this.handleComposition.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.valueChange = this.valueChange.bind(this);
+    }
+
+    valueChange(value){
+        let {onChange} = this.props;
+        onChange&&onChange(value);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.value !== this.props.value) {
+            this.setState({value: nextProps.value});
+        }
+    }
+
+    handleComposition(e) {
+        let type = e.type;
+        if (type === 'compositionend') {
+            this.onComposition = false
+        } else if(type === 'compositionstart'|| type === 'compositionupdate'){//React 14.X版本的input在ie8下keyup,keydown也会触发composition事件
+            this.onComposition = true
+        }
+    }
+
+    handleChange(value) {
+        this.setState({value: value},() => {
+            if (this.onComposition) return;
+            this.valueChange(value);
+        })
+    }
+
+    render() {
+        const { tagName, refc,...other,} = this.props;
+        const Tag = tagName;
+        return (
+            <Tag
+                {...other}
+                onCompositionStart={this.handleComposition}
+                onCompositionUpdate={this.handleComposition}
+                onCompositionEnd={this.handleComposition}
+                ref={c => {
+                  refc(c)
+                  return c
+                }}
+                onChange={(e) => {
+                  this.handleChange(e.target.value)
+                }}
+                value={this.state.value}
+                ></Tag>
+        )
+    }
+}
+
+Input.defaultProps = {
+    tagName: 'input',
+    type: 'text',
+    value: '',
+    onChange: function (value) {
+    },
+    refc: function (c) {
+    },
+};
+
+export default Input
